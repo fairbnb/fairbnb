74b51438736f52d678d9640fe5b57ceb2e6a1892
Erez Levanon <erez.leavnon@mail.huji.ac.il>
2015-12-08 01:16:37 +0200

    more tests

--- static/js/mainJs.js
+++ static/js/mainJs.js
@@ -41,30 +41,7 @@ $('#submitSearch').click(jqxhr
 
 var jqxhr = $.get( "/search", function(data) {
   alert( "success" );
-    alert (data)////    var checkIn = document.getElementById("CheckIn").value;
-////    var checkOut = document.getElementById("CheckOut").value;
-////    checkIn = checkIn + ' 00:00:00';
-////    checkOut = checkOut + ' 00:00:00';
-////    checkIn = moment(checkIn).unix();
-////    checkOut = moment(checkOut).unix();
-////    checkIn += 86400;
-////    checkOut += 86400;
-////
-////    document.getElementById("in").innerHTML = checkIn;
-////    document.getElementById("out").innerHTML = checkOut;
-////
-////    $.get( "/search", { checkIn: checkIn, checkOut: checkOut } ,function(res){
-////        var inner = "";
-////        alert("hi")
-////        $.each(res["results"],function(i, value)
-////        {
-////            alert("get request")
-////            alert(value)
-////            inner += "<div class='row'><div class='col-lg-12' align='center'><h4>" + value.sdate + "</h4></div></div>";
-////            inner += "<div class='row'><div class='col-lg-5' align='right'><div class='row'><h4>" + value.name + "</h4></div><div class='row'><h4>" + value.price + "</h4></div></div><div class='col-lg-7' align='left'><img src='" + value.imurl + "' width='180' height='160' alt=''/></div></div>"
-////        });
-////        document.getElementById("resultContainer").innerHTML=inner;
-////    });
+
 ////    alert("bi")
 ////
 })
@@ -88,6 +65,29 @@ var jqxhr = $.get( "/search", function(data) {
 jqxhr.always(function(data) {
   alert( "second finished" );
     alert(data)
+        alert (data)
+
+    var checkOut = document.getElementById("CheckOut").value;
+    checkIn = checkIn + ' 00:00:00';
+    checkOut = checkOut + ' 00:00:00';
+    checkIn = moment(checkIn).unix();
+    checkOut = moment(checkOut).unix();
+    checkIn += 86400;
+    checkOut += 86400;
+
+    document.getElementById("in").innerHTML = checkIn;
+    document.getElementById("out").innerHTML = checkOut;
+
+        var inner = "";
+        alert("hi")
+        $.each(data,function(i, value)
+        {
+            alert("get request")
+            alert(value)
+            inner += "<div class='row'><div class='col-lg-12' align='center'><h4>" + value.sdate + "</h4></div></div>";
+            inner += "<div class='row'><div class='col-lg-5' align='right'><div class='row'><h4>" + value.name + "</h4></div><div class='row'><h4>" + value.price + "</h4></div></div><div class='col-lg-7' align='left'><img src='" + value.imurl + "' width='180' height='160' alt=''/></div></div>"
+        });
+        document.getElementById("resultContainer").innerHTML=inner;
 });
 
 
