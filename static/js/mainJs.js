be3dcfea9b1f9eda8e2c95801400e42e88e21292
Erez Levanon <erez.leavnon@mail.huji.ac.il>
2015-12-08 01:11:53 +0200

    more tests

--- static/js/mainJs.js
+++ static/js/mainJs.js
@@ -39,41 +39,55 @@ $('#submitSearch').click(jqxhr
 ////}
 );
 
-var jqxhr = $.get( "/search", function(res) {
+var jqxhr = $.get( "/search", function(data) {
   alert( "success" );
-    alert('printing res')
-    alert (res)
-    var checkIn = document.getElementById("CheckIn").value;
-    var checkOut = document.getElementById("CheckOut").value;
-    document.getElementById("in").innerHTML = checkIn;
-    document.getElementById("out").innerHTML = checkOut;
-    var inner = "";
-        var res2 = $.parseJSON(res);
-        alert(res2);
-        $.each(res2["results"],function(i, value)
-        {
-            alert("get request");
-            alert(value);
-            inner += "<div class='row'><div class='col-lg-12' align='center'><h4>" + value.sdate + "</h4></div></div>";
-            inner += "<div class='row'><div class='col-lg-5' align='right'><div class='row'><h4>" + value.name + "</h4></div><div class='row'><h4>" + value.price + "</h4></div></div><div class='col-lg-7' align='left'><img src='" + value.imurl + "' width='180' height='160' alt=''/></div></div>"
-        });
-        document.getElementById("resultContainer").innerHTML=inner;
-
+    alert (data)////    var checkIn = document.getElementById("CheckIn").value;
+////    var checkOut = document.getElementById("CheckOut").value;
+////    checkIn = checkIn + ' 00:00:00';
+////    checkOut = checkOut + ' 00:00:00';
+////    checkIn = moment(checkIn).unix();
+////    checkOut = moment(checkOut).unix();
+////    checkIn += 86400;
+////    checkOut += 86400;
+////
+////    document.getElementById("in").innerHTML = checkIn;
+////    document.getElementById("out").innerHTML = checkOut;
+////
+////    $.get( "/search", { checkIn: checkIn, checkOut: checkOut } ,function(res){
+////        var inner = "";
+////        alert("hi")
+////        $.each(res["results"],function(i, value)
+////        {
+////            alert("get request")
+////            alert(value)
+////            inner += "<div class='row'><div class='col-lg-12' align='center'><h4>" + value.sdate + "</h4></div></div>";
+////            inner += "<div class='row'><div class='col-lg-5' align='right'><div class='row'><h4>" + value.name + "</h4></div><div class='row'><h4>" + value.price + "</h4></div></div><div class='col-lg-7' align='left'><img src='" + value.imurl + "' width='180' height='160' alt=''/></div></div>"
+////        });
+////        document.getElementById("resultContainer").innerHTML=inner;
+////    });
+////    alert("bi")
+////
 })
-  .done(function() {
+  .done(function(data) {
+    alert( "second success" );
+      alert(data)
+
   })
-  .fail(function() {
+  .fail(function(data) {
     alert( "error" );
+      alert(data)
   })
-  .always(function() {
+  .always(function(data) {
     alert( "finished" );
+      alert(data)
   });
 
 // Perform other work here ...
 
 // Set another completion function for the request above
-jqxhr.always(function() {
+jqxhr.always(function(data) {
   alert( "second finished" );
+    alert(data)
 });
 
 
