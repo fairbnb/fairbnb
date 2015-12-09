35033aef05c7db586610bcc1b6d80835c359e874
dermersean <dermersean@gmail.com>
2015-12-09 18:08:04 +0200

    last design change

--- static/js/mainJs.js
+++ static/js/mainJs.js
@@ -19,12 +19,29 @@ $('#submitSearch').click(function()
         var inner = "";
         var price;
         var curRes;
+        var totalPrice = 0;
         for (var i = 0; i < jsonLen; i++){
-                curRes = listings[i];
-                inner += "<div class='row'><div class='col-lg-12' align='center' style='margin-bottom: 20px'><h6>" + curRes['sdate'] + "</h6></div></div>";
-                inner += "<div class='row'><div class='col-lg-5' align='right' style='margin-top: 45px'><div class='row'><h4>$" + curRes['price'] + "</h4></div></div><div class='col-lg-7' align='left' style='margin-bottom: 20px'><a href='"+curRes['href']+"' target='_blank'><img class='img-circle' src='" + curRes['imurl'] + "' width='180' height='160' alt=''/></a></div></div>"
+            curRes = listings[i]
+            totalPrice += parseInt(curRes['price']);
+            if (i%2){
+            inner += "<li><div class='timeline-badge'></div><div class='timeline-panel'><div class='timeline-heading'>"
+                   + "<h4 class='timeline-title'>"+curRes['sdate'] +" to " + curRes['edate']+"</h4></div><div class='timeline-body'><h4>"+curRes['days']+" days</h4><h4>$"+ curRes['price'] +"</h4>"
+                +"<a href='"+curRes['href']+"' target='_blank'><img class='img-rounded' src='" + curRes['imurl'] + "' width='180' height='160' alt=''/></a></div></div></li>";
+            }
+            else{
+                inner += "<li class='timeline-inverted'><div class='timeline-badge'></div><div class='timeline-panel'><div class='timeline-heading'>"
+                    + "<h4 class='timeline-title'>"+curRes['sdate'] +" to " + curRes['edate']+"</h4></div><div class='timeline-body'><h4>"+curRes['days']+" days</h4><h4>$"+ curRes['price'] +"</h4>"
+                    +"<a href='"+curRes['href']+"' target='_blank'><img class='img-rounded' src='" + curRes['imurl'] + "' width='180' height='160' alt=''/></a>";
+
+            }
+
+                //inner += "<div class='row'><div class='col-lg-12' align='center' style='margin-bottom: 20px'><h6>" + curRes['sdate'] + "</h6></div></div>";
+                //inner += "<div class='row'><div class='col-lg-5' align='right' style='margin-top: 45px'><div class='row'><h4>$" + curRes['price'] + "</h4></div></div><div class='col-lg-7' align='left' style='margin-bottom: 20px'><a href='"+curRes['href']+"' target='_blank'><img class='img-circle' src='" + curRes['imurl'] + "' width='180' height='160' alt=''/></a></div></div>"
         }
-        inner += "<div class='row'><div class='col-lg-12' align='center'><h6>" + listings[jsonLen-1]['edate'] + "</h6></div></div>";
+        //inner += "<div class='row'><div class='col-lg-12' align='center'><h6>" + listings[jsonLen-1]['edate'] + "</h6></div></div>";
+        inner+="<li class='timeline-inverted'><div class='timeline-badge'></div><div class='timeline-panel'><div class='timeline-heading'>"
+            + "<h4 class='timeline-title'>Total price: $"+ totalPrice +"</h4></div>";
+        //inner+="<div class='row'><h6>Total price: $"+ totalPrice +"</h6></div>";
         document.getElementById("resultContainer").innerHTML=inner;
     });
 
@@ -53,4 +70,17 @@ $(document).on({
         $('html, body').animate({scrollTop:950},1000);
     }
 });
+
+var d = new Date();
+
+var month = d.getMonth()+1;
+var day = d.getDate();
+
+var curDate = d.getFullYear() + '-' +
+    (month<10 ? '0' : '') + month + '-' +
+    (day<10 ? '0' : '') + day;
+document.getElementById("CheckIn").defaultValue = curDate;
+document.getElementById("CheckOut").defaultValue = curDate;
+
+
 //result();
